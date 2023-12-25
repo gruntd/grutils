2e8e9895d9131e7e366e81d304c3229b6b5c8759
Jost Grant <src.grant@pm.me>
2023-12-24 19:44:49 -0600

    Okay, more loose notes for rino

--- stream/speak/rino/rino.lisp
+++ stream/speak/rino/rino.lisp
@@ -4,3 +4,14 @@
 ;; (using sez and even play audio via it)
 ;; when certain stream events happen or just
 ;; in general when someone says something in chat.
+
+;; We want to also basically have a 'trusted user'
+;; whitelist we can add people to that have access
+;; to sez to read whatever. 
+
+;; A list of banned words.
+;; 'Trigger words' that trigger events in the bot.
+;; etc, etc.
+
+;; Basically be the start of stubbing out botl, 
+;; well ... at least conceptually. 
