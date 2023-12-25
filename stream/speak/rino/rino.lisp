71379baacb3a56470599e656767f53695e83e053
Jost Grant <src.grant@pm.me>
2023-12-24 19:42:06 -0600

    Add a note about rino

---
+++ stream/speak/rino/rino.lisp
@@ -0,0 +1,6 @@
+;; add chatterino log watching
+;; should watch latest line
+;; and be able to say outloud 
+;; (using sez and even play audio via it)
+;; when certain stream events happen or just
+;; in general when someone says something in chat.
